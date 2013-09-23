3944034e2a7ceed7b27218d36e338ae36f55065a
Nikolay A. Mishin <mi@ya.ru>
2013-09-23 12:42:32 +0400

    make init data

---
+++ t1.pl
@@ -0,0 +1,38 @@
+#!/usr/bin/env perl
+#!perl
+use Modern::Perl;
+use DDP;
+my $data = &read_DATA( join '', <DATA> );
+my $golf= $data->{Perl_Golf};
+# p $data;
+p $golf;
+
+
+sub read_DATA {
+
+    # my ($file) = @_;
+    my $string = shift; #read_file($file);
+    print {*STDERR} "Reading from __DATA__...\n";
+
+  # my %contents_of = do { local $/; "", split /_____\[ (\S+) \]_+\n/, <DATA> };
+    my %contents_of =
+      do { local $/; "", split /_____\[ (\S+) \]_+\n/, $string };
+    for ( values %contents_of ) {
+        s/^!=([a-z])/=$1/gxms;
+    }
+    print {*STDERR} "done\n";
+    return \%contents_of;
+}
+__END__
+__DATA__
+_____[ Perl_Golf ]________________________________________________
+         
+         
+         
+         
+         
+         
+         
+         
+         
+       bw
