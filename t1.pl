9ae1e0836cf069460c7e38a26f67a3dff30d653f
Nikolay A. Mishin <mi@ya.ru>
2013-09-23 15:22:51 +0400

    добавил некторые идеи

--- t1.pl
+++ t1.pl
@@ -1,7 +1,10 @@
 #!/usr/bin/env perl
 #!perl
+# http://pointsgame.narod.ru/
 use Modern::Perl;
+use utf8;
 use DDP;
+
 my $data = &read_DATA( join '', <DATA> );
 my @golf = split /\n/, $data->{'Perl_Golf'};
 my @data;
@@ -20,7 +23,8 @@ sub read_DATA {
     my %contents_of =
       do { local $/; "", split /_____\[ (\S+) \]_+\n/, $string };
     for ( values %contents_of ) {
-        s/^!=([a-z])/=$1/gxms;
+        # s/^!=([a-z])/=$1/gxms;
+        s/^!=(\w+)/=$1/gxms;
     }
     print {*STDERR} "done\n";
     return \%contents_of;
@@ -38,3 +42,8 @@ _____[ Perl_Golf ]________________________________________________
          
          
        bw
+_____[ Идеи_и_Решения ]________________________________________________
+Решение задачи Го
+1) берем белую точку и измеряем расстяния до черных в пределах 
+2) цепь замкнута, то есть каждая из точек имеет соседнюю.
+3) Среднее расстояние меньше, чем половина расстояния до самой дальней
